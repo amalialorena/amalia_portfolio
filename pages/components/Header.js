6241a71fdbb263fb8f2c69ac24358fa87844fb84
Amalia <amalialorenaferariu@gmail.com>
2022-02-04 17:26:04 +0100

    header

--- pages/components/Header.js
+++ pages/components/Header.js
@@ -1,17 +1,20 @@
 function Header() {
     return (
       <header>
+       <div className="greeting-bubble">
        <h1>Hi,</h1>
+       </div>
        <span>
          My name is <span className="name">Amalia</span>,
        </span>
        <span>
-         I am a junior <span>Frontend Developer</span>  based in Lecco
+         I am a junior <span>Frontend Developer</span>  based in Lecco.
        </span>
+       <img src="http:/available-dot.svg" alt="avaliability dot"></img>
        <p>
          Right now I am available for work
        </p>
-       <button>contact me</button>
+       <button>Contact me</button>
       </header>
     );
   }
