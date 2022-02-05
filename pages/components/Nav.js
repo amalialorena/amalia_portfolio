25aded6d52a5361b71ba680817f2dfe4c7696874
Amalia <amalialorenaferariu@gmail.com>
2022-02-05 09:49:37 +0100

    added nav logo

--- pages/components/Nav.js
+++ pages/components/Nav.js
@@ -1,14 +1,25 @@
+import Image from "next/image";
+
 function Nav() {
   return (
     <nav>
-      <a className="name">
-        <div>Amalia Lorena Ferariu</div>
-        <div className="portfolio">Personal portfolio</div>
-      </a>
-      <div>
-          <button className="greeting-button">Contact me</button>
+      <div className="name-container">
+        <div className="logo">
+          <Image src="/logo.svg" alt="logo" width="29" height="29" />
+        </div>
+        <a>
+          <div className="my-name">
+            <strong>Amalia</strong> Lorena Ferariu
+          </div>
+          <div className="portfolio">Personal portfolio</div>
+        </a>
       </div>
-      
+      <div>
+        <button className="greeting-button">
+          <Image src="/mail.svg" alt="mail" width="16" height="16" />
+          <span>Contact me</span>
+        </button>
+      </div>
     </nav>
   );
 }
