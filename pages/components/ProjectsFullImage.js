926283efa304784acf03ae949a135692375c70aa
Amalia <amalialorenaferariu@gmail.com>
2022-02-06 08:43:38 +0100

    changed tags

--- pages/components/ProjectsFullImage.js
+++ pages/components/ProjectsFullImage.js
@@ -1,73 +1,98 @@
-import Image from 'next/image';
+import Image from "next/image";
 
 const projects = [
-    {
-        id: '1',
-        tags: 'VUEJS',
-        title: 'BoolFlix: a Netflix clone',
-        url: 'https://serene-aryabhata-f2a958.netlify.app',
-        description: 'Lorem Ipsum è un testo segnaposto utilizzato nel settore della tipografia e della stampa. Lorem Ipsum è considerato il testo segnaposto standard sin dal sedicesimo secolo.',
-        imagePath: '/boolflix.jpg',
-        imageAlt: 'project image',
-        imageWidth: '650',
-        imageHeight: '325',
-    }, 
-    {
-        id: '2',
-        tags: 'VUEJS',
-        title: 'BoolApp: a whatsapp clone',
-        url: 'https://elastic-rosalind-d13b9f.netlify.app',
-        description: 'Lorem Ipsum è un testo segnaposto utilizzato nel settore della tipografia e della stampa. Lorem Ipsum è considerato il testo segnaposto standard sin dal sedicesimo secolo.',
-        imagePath: '/boolzapp.jpg',
-        imageAlt: 'project image',
-        imageWidth: '650',
-        imageHeight: '325',
-    },
-    {
-        id: '3',
-        tags: 'VUEJS',
-        title: 'BoolApp: a whatsapp clone',
-        url: 'https://optimistic-blackwell-b8d3cc.netlify.app',
-        description: 'Lorem Ipsum è un testo segnaposto utilizzato nel settore della tipografia e della stampa. Lorem Ipsum è considerato il testo segnaposto standard sin dal sedicesimo secolo.',
-        imagePath: '/playstation.jpg',
-        imageAlt: 'project image',
-        imageWidth: '650',
-        imageHeight: '325',
-    },
-    {
-        id: '4',
-        tags: 'VUEJS',
-        title: 'BoolApp: a whatsapp clone',
-        url: 'https://quizzical-benz-9502d1.netlify.app',
-        description: 'Lorem Ipsum è un testo segnaposto utilizzato nel settore della tipografia e della stampa. Lorem Ipsum è considerato il testo segnaposto standard sin dal sedicesimo secolo.',
-        imagePath: '/spotify.jpg',
-        imageAlt: 'project image',
-        imageWidth: '650',
-        imageHeight: '325',
-    },
+  {
+    id: "1",
+    tags: ["VUEJS", "HTML", "SCSS"],
+    title: "BoolFlix: a Netflix clone",
+    url: "https://serene-aryabhata-f2a958.netlify.app",
+    description:
+      "Lorem Ipsum è un testo segnaposto utilizzato nel settore della tipografia e della stampa. Lorem Ipsum è considerato il testo segnaposto standard sin dal sedicesimo secolo.",
+    imagePath: "/boolflix.jpg",
+    imageAlt: "project image",
+    imageWidth: "650",
+    imageHeight: "325",
+  },
+  {
+    id: "2",
+    tags: ["VUEJS", "HTML", "SCSS"],
+    title: "BoolApp: a whatsapp clone",
+    url: "https://elastic-rosalind-d13b9f.netlify.app",
+    description:
+      "Lorem Ipsum è un testo segnaposto utilizzato nel settore della tipografia e della stampa. Lorem Ipsum è considerato il testo segnaposto standard sin dal sedicesimo secolo.",
+    imagePath: "/boolzapp.jpg",
+    imageAlt: "project image",
+    imageWidth: "650",
+    imageHeight: "325",
+  },
+  {
+    id: "3",
+    tags: ["VUEJS", "HTML", "SCSS"],
+    title: "BoolApp: a whatsapp clone",
+    url: "https://optimistic-blackwell-b8d3cc.netlify.app",
+    description:
+      "Lorem Ipsum è un testo segnaposto utilizzato nel settore della tipografia e della stampa. Lorem Ipsum è considerato il testo segnaposto standard sin dal sedicesimo secolo.",
+    imagePath: "/playstation.jpg",
+    imageAlt: "project image",
+    imageWidth: "650",
+    imageHeight: "325",
+  },
+  {
+    id: "4",
+    tags: ["VUEJS", "HTML", "SCSS"],
+    title: "BoolApp: a whatsapp clone",
+    url: "https://quizzical-benz-9502d1.netlify.app",
+    description:
+      "Lorem Ipsum è un testo segnaposto utilizzato nel settore della tipografia e della stampa. Lorem Ipsum è considerato il testo segnaposto standard sin dal sedicesimo secolo.",
+    imagePath: "/spotify.jpg",
+    imageAlt: "project image",
+    imageWidth: "650",
+    imageHeight: "325",
+  },
 ];
 
 function ProjectsFullImage() {
-    return (
-      <section className="cards-container">
-       <h2>Projects</h2>
-       <div className="cards">
-            {projects.map((project) => {
-                return <div className="card" key={project.id}>
-                    <Image src={project.imagePath} alt={project.imageAlt} width={project.imageWidth} height={project.imageHeight}/>
-                    <div className="text">
-                        <div className="tags">{project.tags}</div>
-                        <div className="title">{project.title}</div>
-                        <div className="description">{project.description}</div>    
-                    </div>
-                    <a href={project.url} target="_blank" rel="noreferrer" className="button">Visit website</a>
+  return (
+    <section className="cards-container">
+      <h2>Projects</h2>
+      <div className="cards">
+        {projects.map((project) => {
+          return (
+            <div className="card" key={project.id}>
+              <Image
+                src={project.imagePath}
+                alt={project.imageAlt}
+                width={project.imageWidth}
+                height={project.imageHeight}
+              />
+              <div className="text">
+                <div className="tags">
+                  {project.tags.map(function (tag) {
+                    return (
+                      <span className="tag" key={tag}>
+                        {tag}
+                      </span>
+                    );
+                  })}
                 </div>
-            })}
-       </div>
-            
-      </section>
-    );
-  };
 
+                <div className="title">{project.title}</div>
+                <div className="description">{project.description}</div>
+              </div>
+              <a
+                href={project.url}
+                target="_blank"
+                rel="noreferrer"
+                className="button"
+              >
+                Visit website
+              </a>
+            </div>
+          );
+        })}
+      </div>
+    </section>
+  );
+}
 
-  export default ProjectsFullImage;
+export default ProjectsFullImage;
