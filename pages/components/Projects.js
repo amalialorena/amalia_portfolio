cb394161f755c1518508ee9e8a69aab469ee2dac
Amalia <amalialorenaferariu@gmail.com>
2022-02-06 17:49:35 +0100

    update project descriptions

--- pages/components/Projects.js
+++ pages/components/Projects.js
@@ -3,11 +3,11 @@ import Image from "next/image";
 const projects = [
   {
     id: "1",
-    tags: ["VUEJS", "HTML", "CSS"],
+    tags: ["VUEJS","JS", "HTML", "CSS"],
     title: "BoolFlix: a Netflix clone",
     url: "https://serene-aryabhata-f2a958.netlify.app",
     description:
-      "Lorem Ipsum è un testo segnaposto utilizzato nel settore della tipografia e della stampa. Lorem Ipsum è considerato il testo segnaposto standard sin dal sedicesimo secolo.",
+      "Boolflix is ​​a project made in Vue.js. It interfaces with the TMDB API to show the user a catalog of Movies and TV Series. It allows you to search  any film or series and read  related pieces of information like original title, rating, overview, language etc.",
     imagePath: "/boolflix.jpg",
     imageAlt: "project image",
     imageWidth: "650",
@@ -15,11 +15,11 @@ const projects = [
   },
   {
     id: "2",
-    tags: ["VUEJS", "HTML", "CSS"],
+    tags: ["VUEJS","JS", "HTML", "CSS", "DayJs"],
     title: "BoolApp: a WhatsApp clone",
     url: "https://elastic-rosalind-d13b9f.netlify.app",
     description:
-      "Lorem Ipsum è un testo segnaposto utilizzato nel settore della tipografia e della stampa. Lorem Ipsum è considerato il testo segnaposto standard sin dal sedicesimo secolo.",
+      "'Boolzapp' is the replica of Whatsapp Web, it allows you to send messages to contacts, and receive an automatic reply after a second. The hour and date display on your messages are updated with DayJs.  You can filter your contacts by entering their name in the search bar or you can select them by clicking on their name. The last message sent or received is displayed under the contact name.",
     imagePath: "/boolzapp.jpg",
     imageAlt: "project image",
     imageWidth: "650",
@@ -31,7 +31,7 @@ const projects = [
     title: "Playstation clone",
     url: "https://optimistic-blackwell-b8d3cc.netlify.app",
     description:
-      "Lorem Ipsum è un testo segnaposto utilizzato nel settore della tipografia e della stampa. Lorem Ipsum è considerato il testo segnaposto standard sin dal sedicesimo secolo.",
+      "This was among my first big projects after a month and a half of training with Boolean Careers. Responsive playback of the official Playstation website. ",
     imagePath: "/playstation.jpg",
     imageAlt: "project image",
     imageWidth: "650",
@@ -43,7 +43,7 @@ const projects = [
     title: "Spotify clone",
     url: "https://quizzical-benz-9502d1.netlify.app",
     description:
-      "Lorem Ipsum è un testo segnaposto utilizzato nel settore della tipografia e della stampa. Lorem Ipsum è considerato il testo segnaposto standard sin dal sedicesimo secolo.",
+      "Spotify replication was the first web app I developed, compared to previous projects the use of Media Queries was also implemented to make the application completely responsive. The project is developed with HTML and CSS.",
     imagePath: "/spotify.jpg",
     imageAlt: "project image",
     imageWidth: "650",
@@ -67,7 +67,7 @@ const projects = [
     title: "Avada theme clone",
     url: "https://optimistic-bassi-1bc253.netlify.app",
     description:
-      "Lorem Ipsum è un testo segnaposto utilizzato nel settore della tipografia e della stampa. Lorem Ipsum è considerato il testo segnaposto standard sin dal sedicesimo secolo.",
+      "Avada theme is a web app built in Vue js. Thanks to the use of components, I was able to reuse portions of the layout, improving the maintainability of the code and the performance of my application. Some elements on the page are interactive like the menu for example.",
     imagePath: "/avada.jpg",
     imageAlt: "project image",
     imageWidth: "650",
@@ -79,7 +79,7 @@ const projects = [
     title: "My art on Instagram",
     url: "https://www.instagram.com/sherbetlemonit/",
     description:
-      "Lorem Ipsum è un testo segnaposto utilizzato nel settore della tipografia e della stampa. Lorem Ipsum è considerato il testo segnaposto standard sin dal sedicesimo secolo.",
+      "In my spare time, I dedicated myself to developing my artistic skills by working on numerous projects ranging from illustration to sculpture. Pictures of my artwork are available on my instagram page.",
     imagePath: "/sherbet-lemon-etsy2.jpg",
     imageAlt: "project image",
     imageWidth: "650",
