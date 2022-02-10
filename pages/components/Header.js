fa3a12941bf674428896f8bc6a9ebaf4520ca918
Amalia <amalialorenaferariu@gmail.com>
2022-02-10 10:02:25 +0100

    added icon animations

--- pages/components/Header.js
+++ pages/components/Header.js
@@ -1,37 +1,76 @@
+import { useRef, useEffect } from "react";
 import Image from "next/image";
-import { mailTo } from '../../utils/utils';
+import { mailTo } from "../../utils/utils";
+import { gsap } from "gsap";
+
+export const fadeIn = (ref) => {
+  gsap.fromTo(
+    ref.current,
+    { autoAlpha: 0 },
+    {
+      autoAlpha: 1,
+      duration: 1,
+      delay: 1,
+      scrollTrigger: {
+        trigger: ref.current,
+        once: false,
+      },
+    }
+  );
+}
 
 function Header() {
+  const instaRef = useRef();
+  const gitRef = useRef();
+  const emailRef = useRef();
+  const hiRef = useRef();
+  const textRef = useRef();
+
+  useEffect(() => {
+    gsap.to(instaRef.current, { rotation: "+=360", duration: 2 });
+    gsap.to(gitRef.current, { rotation: "+=360", duration: 2 });
+    gsap.to(emailRef.current, { rotation: "+=360" });
+
+    gsap.fromTo(
+      hiRef.current,
+      { autoAlpha: 0, y: -10, scale: 0 },
+      {
+        autoAlpha: 1,
+        y: 0,
+        scale: 1,
+        duration: 1,
+        // scrollTrigger: {
+        //   trigger: hiRef.current,
+        // },
+      }
+    );
+
+    fadeIn(textRef);
+  });
+
   return (
-    <header>     
-      <div className="greeting-bubble">
-      <a href="https://www.instagram.com/sherbetlemonit/" className="social-bubble social-instagram">
-          <Image
-            src="/instagram.svg"
-            alt="instagram"
-            width="32"
-            height="32"
-          />
-      </a>
-      <a href="https://github.com/amalialorena" className="social-bubble social-github">
-          <Image
-            src="/github.svg"
-            alt="github"
-            width="32"
-            height="32"
-          />
-      </a>
-      <a href={mailTo} className="social-bubble social-email">
-          <Image
-            src="/email.svg"
-            alt="email"
-            width="32"
-            height="32"
-          />
-      </a>
+    <header>
+      <div className="greeting-bubble" ref={hiRef}>
+        <a
+          href="https://www.instagram.com/sherbetlemonit/"
+          className="social-bubble social-instagram"
+          ref={instaRef}
+        >
+          <Image src="/instagram.svg" alt="instagram" width="32" height="32" />
+        </a>
+        <a
+          href="https://github.com/amalialorena"
+          className="social-bubble social-github"
+          ref={gitRef}
+        >
+          <Image src="/github.svg" alt="github" width="32" height="32" />
+        </a>
+        <a href={mailTo} className="social-bubble social-email" ref={emailRef}>
+          <Image src="/email.svg" alt="email" width="32" height="32" />
+        </a>
         <h2>Hi,</h2>
       </div>
-      <div className="greeting-text">
+      <div className="greeting-text" ref={textRef}>
         <span>
           My name is <span className="name">Amalia</span>,
         </span>
@@ -49,12 +88,7 @@ function Header() {
       />
       <p>Right now I am available for work</p>
       <a href={mailTo} className="button greeting-button">
-        <Image
-        src="/mail.svg"
-        alt="mail"
-        width="16"
-        height="16"
-        />
+        <Image src="/mail.svg" alt="mail" width="16" height="16" />
         <span>Contact me</span>
       </a>
     </header>
--- pages/components/Intro.js
+++ pages/components/Intro.js
@@ -1,6 +1,29 @@
-const Intro = () => {
+import { useRef, useEffect } from 'react';
+import { gsap } from "gsap";
+
+
+function Intro() {
+  const introRef = useRef();
+
+  useEffect(() => {
+    gsap.fromTo(
+      introRef.current,
+      { autoAlpha: 0 },
+      {
+        autoAlpha: 1,
+        duration: 1,
+        delay: 1,
+        scrollTrigger: {
+          trigger: document.querySelector('.intro'),
+          once: false,
+          markers: true
+        },
+      }
+    );
+  });
+
   return (
-    <div className="intro">
+    <div className="intro" ref={introRef} >
       I am creative, attentive to details with a passion for illustration,
       programming and animation, I dream of being able to help create solutions
       that leave a positive mark on the world. I gained experience with various
